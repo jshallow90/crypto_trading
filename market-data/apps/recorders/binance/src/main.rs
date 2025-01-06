14b983eb38853226ff105af18e7ea905a97f50e8
Josh Shallow <josh.shallow@gmail.com>
2025-01-06 22:14:42 +0000

    refactoring to use internal libs

--- market-data/apps/recorders/binance/src/main.rs
+++ market-data/apps/recorders/binance/src/main.rs
@@ -1,3 +1,25 @@
+use recorders::Config;
+use tungstenite::connect;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    /* TODO list
+        - create a struct for the websocket reader
+        - start up listener for multiple symbols
+        - track the last traded price (inside bid / ask)
+     */
+    
+    let config = Config::from_file("/home/jshallow/src/crypto-trading/market-data/apps/recorders/binance/src/config.yaml");
+    let api_url = config.get_api_url();
+    
+    let (mut socket, response) = connect(api_url).expect("Can't connect");
+    println!("Connecting to the server");
+    println!("Response HTTP code: {}", response.status());
+
+    loop {
+        let msg = socket.read().expect("Error reading message");
+        println!("Received: {msg}");
+    }
+
 }
